b23a12540a784fa198460d4999d7b8bf62db30dc
eumenid3s <eumenid3s@e17a4ed8-952f-0410-b464-01dc69fc9241>
2007-05-17 08:17:48 +0000

    dum di dum

--- trunk/hImgb.hs
+++ trunk/hImgb.hs
@@ -1,14 +1,21 @@
+{-# OPTIONS_GHC -fbang-patterns #-}
 module Main where
 
 import Network.CGI
-import Control.Monad
 import Graphics.GD
 import Text.XHtml
+
 import qualified Data.ByteString.Lazy as B
-import Data.Maybe
 import Data.Char
+
 import System.Posix.Time
+import System.Environment
 import System.IO
+
+import Control.Monad
+import Control.Monad.STM
+import Control.Concurrent
+import Control.Concurrent.STM.TMVar
 
 data Post = Post { up_name :: String
                  , file    :: Maybe String
@@ -85,33 +92,31 @@ formatThread t = hr +++
 renderimgb :: HState -> Html
 renderimgb h = (thetitle (toHtml (ibtitle h))) +++
                h1 (toHtml (ibtitle h)) +++ 
+               h2 (toHtml "broken so far: once") +++
                hr +++
                replyForm "1" +++
                (foldr ((+++).formatThread) noHtml $ threads h) +++
-               (p $ toHtml $ lnk "source" "hImgb.hs")
+               (p $ toHtml $ lnk "source" "http://code.google.com/p/himgb/")
 
 lnk txt hrf = anchor (toHtml txt) ! [href hrf]
 
 -- ------------------------------------------------------------------------
 
 makethumb :: FilePath -> IO ()
-makethumb file = do
+makethumb !file = do
     t <- getFileType file
     img <- case t of ".jpg" -> loadJpegFile file
                      ".gif" -> loadGifFile  file
                      ".png" -> loadPngFile  file
-                     _      -> fail $ "unrecognised image format." ++ 
-                                      "supported: jpg, png, gif"
     (sizex, sizey) <- imageSize img
-    let ratiox = fromIntegral sizex / 300.0
-        ratioy = fromIntegral sizey / 300.0
+    let ratiox = fromIntegral sizex / 100.0
+        ratioy = fromIntegral sizey / 100.0
         ratio = max ratiox ratioy
     img' <- resizeImage (round (fromIntegral sizex / ratio))
                         (round (fromIntegral sizey / ratio)) img
     case t of ".jpg" -> saveJpegFile (-1) (fth file) img'
               ".png" -> savePngFile       (fth file) img'
               ".gif" -> saveGifFile       (fth file) img'
-              _      -> fail "this should never happen?"
 
 mkPost :: String -> Maybe String -> String -> String -> Post
 mkPost n f u t = Post { up_name = take 20 (filter isAscii n)
@@ -143,36 +148,57 @@ getFileType s = let fff = map toLower (reverse s) in
         ('g':'p':'j':_)     -> ".jpg"
         ('f':'i':'g':_)     -> ".gif"
         ('g':'n':'p':_)     -> ".png"
-        _                   -> fail "unknown file type"
+        _                   -> "unknown"
+
+-- ------------------------------------------------------------------------
+
+compute_ :: Int -> IO a -> IO (Maybe a)
+compute_ limit computation = do
+    result <- atomically newEmptyTMVar
+    runner <- forkIO $ do c <- computation
+                          atomically $ putTMVar result $ Just c
+    reader <- forkIO $ do threadDelay limit
+                          killThread runner
+                          atomically $ putTMVar result $ Nothing
+    a <- atomically $ takeTMVar result
+    killThread runner
+    killThread reader
+    return a
+
+compute :: IO () -> IO ()
+compute fn = compute_ (5*10^6) fn >> return ()
 
 -- ------------------------------------------------------------------------
 
 main :: IO ()
-main = do
-    runCGI (handleErrors imgb)
-
+main = runCGI (handleErrors imgb)
+ 
 imgb :: CGI CGIResult
 imgb = do
-    efile   <- liftIO $ openFile "stateh" ReadWriteMode
-    idx     <- liftM  (fromMaybe               "") $ getInput "id"
-    text    <- liftM  (fromMaybe        "silence") $ getInput "txt"
-    bslfile <- liftM  (fromMaybe          B.empty) $ getInputFPS "upfile"
-    fname   <- liftM  (fromMaybe               "") $ getInputFilename "upfile"
-    name    <- liftM  (fromMaybe "Anonymous Hero") $ getInput "name"
-    purge   <- liftM  (fromMaybe              "f") $ getInput "purge"
+    args    <- liftIO getArgs
 
-    hst   <- (case purge of 
-      "t" -> return $ HState "hImgB" []
-      _   -> liftM read (liftIO (hGetLine efile)))
+    efile   <- liftIO $ openFile "stateh" ReadWriteMode
+    idx     <- liftM  (maybe               "" id) $ getInput "id"
+    text    <- liftM  (maybe        "silence" id) $ getInput "txt"
+    bslfile <- liftM  (maybe          B.empty id) $ getInputFPS "upfile"
+    fname   <- liftM  (maybe               "" id) $ getInputFilename "upfile"
+    name    <- liftM  (maybe "Anonymous Hero" id) $ getInput "name"
+
+    hst   <- (case args of 
+      ("purge":_) -> return $ HState "hImgB" []
+      _           -> liftM read (liftIO (hGetLine efile)))
     ftype <- liftIO (getFileType fname)
     time  <- liftM show (liftIO epochTime)
 
     case null idx of
       True  -> do
-        liftIO (hClose efile)
+        liftIO $ do overwriteConfig efile hst
+                    hClose efile
         output $ showHtml $ renderimgb hst
 
-      False ->
+      False -> do
+        when (ftype == "unknown")
+          (fail "images only pls")
         case read idx of
           1 ->
             if B.null bslfile
@@ -180,7 +206,7 @@ imgb = do
               else do liftIO $ do
                         let img = "src/"++time++ftype
                         B.writeFile img bslfile
-                        makethumb img
+                        compute $ makethumb img
                         let nt = newThread hst (mkPost
                                   name (Just img) time text)
                         overwriteConfig efile nt
@@ -199,7 +225,7 @@ imgb = do
                               overwriteConfig efile nt
                       else do let img = "src/"++time++ftype
                               B.writeFile img bslfile
-                              makethumb img
+                              compute $ makethumb img
                               let nt = addPost' hst (read idx) (mkPost
                                         name  (Just img) time  text)
                               overwriteConfig efile nt
